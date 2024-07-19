27d6b059d8f567093de395ff164e12283ff47445
Mert Engin <145940439+mertengin1@users.noreply.github.com>
2024-07-19 11:08:00 +0300

    Add files via upload

---
+++ Değişkenler.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Merhaba Mert");
+
+
+
+public class değişkenler : Monobehaviour
+
+{
+    string yazi_atama = "mert";
+    int düz_sayi_atama = 100;
+    float küsüratli_sayi_atama = 1.80f;
+    double uzun_küsüratli_sayi_atama = 1.543543;
+    bool durum_atama = false;
+    bool durum_atama2 = true;
+
+}
